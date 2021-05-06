6b50180a94655c4e144334ca9e66168d23e0ef9e
sandro-silva <68612228+sandro-silva@users.noreply.github.com>
2021-05-06 17:39:04 -0300

    Add files via upload

---
+++ CategoriaProduto.java
@@ -0,0 +1,6 @@
+package br.ufpb.dcx.sistemacomercial;
+
+public enum CategoriaProduto {
+	ALIMENTO, ROUPA, PRODUTO_DE_LIMPEZA, DEFAULT
+
+}
---
+++ ClientePF.java
@@ -0,0 +1,15 @@
+package br.ufpb.dcx.sistemacomercial;
+
+public class ClientePF extends Cliente {
+	private String CPF;
+
+	public ClientePF(String nome, String endereco, String email, String CPF) {
+		super(nome, endereco, email);
+		this.CPF = CPF;
+	}
+
+	@Override
+	public String getId() {
+		return this.CPF;
+	}
+}

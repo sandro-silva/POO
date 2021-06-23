defac447ad35c815a06a2eacb4eb420dfde72cf1
sandro-silva <68612228+sandro-silva@users.noreply.github.com>
2021-06-23 16:26:11 -0300

    Add files via upload

---
+++ Sistemas/SistemaProdutosEletronicos/testes/SistemaEletronicoTest.java
@@ -0,0 +1,26 @@
+package br.ufpb.dcx.projetoeletronico;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SistemaEletronicoTest {
+
+	Produto novoProduto;
+	SistemaEletronico sistema;
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		this.novoProduto = new Produto("Celular","Motorola");
+		this.sistema = new SistemaEletronico();
+	}
+
+	@Test
+	void test() {
+		assertTrue(novoProduto.getNome().equals("Celular"));
+		assertEquals("Celular",novoProduto.getNome());
+		//this.sistema.  Continuar
+	}
+
+}

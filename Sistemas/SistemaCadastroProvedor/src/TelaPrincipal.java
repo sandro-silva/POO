39807893e215fc58536f8319cd36841ff6554085
sandro-silva <68612228+sandro-silva@users.noreply.github.com>
2021-07-04 18:57:59 -0300

    Update TelaPrincipal.java

--- Sistemas/SistemaCadastroProvedor/src/TelaPrincipal.java
+++ Sistemas/SistemaCadastroProvedor/src/TelaPrincipal.java
@@ -1,4 +1,4 @@
-package aluno;
+package br.ufpb.dcx.SistemaCadastroProvedor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,10 +15,10 @@ public class TelaPrincipal extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	JPanel p = new JPanel();  // bot„o ok
+	JPanel p = new JPanel();  // bot√£o ok
 	JButton ok = new JButton("ok");
 	JPanel panel = new JPanel();//Jpanel(abas)
-	JMenuBar menuBar = new JMenuBar(); //barra menu  inst‚nciado
+	JMenuBar menuBar = new JMenuBar(); //barra menu  inst√¢nciado
 	JMenu arquivo = new JMenu("Arquivo");
 	JMenuItem itemCadastrar = new JMenuItem("Novo Cadastro"); //sub menu
 	JMenuItem itemPesquisar = new JMenuItem("consulta Cadastro");
@@ -70,14 +70,14 @@ public class TelaPrincipal extends JFrame{
 			}
 		});
 		arquivo.add(itemPesquisar);
-		//------------------------aÁ„o menu item sair-----------------------------
-		itemSair.addActionListener(new ActionListener() { // aÁ„o pra sair
+		//------------------------a√ß√£o menu item sair-----------------------------
+		itemSair.addActionListener(new ActionListener() { // a√ß√£o pra sair
 			public void actionPerformed(ActionEvent e) { // qnd clicar no botao executa abaixo comando
 				System.exit(0);
 			}
 		}); 
 		arquivo.add(itemSair); 
-		//----------------------------aÁ„o bot„o sair-----------------------------
+		//----------------------------a√ß√£o bot√£o sair-----------------------------
 		menuBar.add(arquivo);
 		setJMenuBar(menuBar);
 		

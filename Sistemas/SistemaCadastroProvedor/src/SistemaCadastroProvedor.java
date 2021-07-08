c1b419e399bd2be4c0ce42fcce4204d6e7b93c6f
puma <puma@puma-PC>
2021-07-07 22:47:16 -0300

    josinaldo

--- Sistemas/SistemaCadastroProvedor/src/SistemaCadastroProvedor.java
+++ Sistemas/SistemaCadastroProvedor/src/SistemaCadastroProvedor.java
@@ -5,7 +5,7 @@ public class SistemaCadastroProvedor {
 	public static void main(String[] args) {
 		Cadastro sisCadastro =  new Cadastro ();
 		TelaCadastro telaCadastro = new TelaCadastro(sisCadastro);
-		//telaCadastro.setVisible(true);oi sandro blz
+		//telaCadastro.setVisible(true);oi josinaldo silva
 		
 		TelaPrincipal  tela1 = new TelaPrincipal (sisCadastro,telaCadastro);
 		tela1.setVisible(true);
